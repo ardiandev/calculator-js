7cbb3c2c8650ec622042d52ca0cd4f3bcfd01fa4
Ardian Pradana <ardianysp18@gmail.com>
2023-12-29 11:05:05 +0700

    Intial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+# Calculator App
+
+This project is part of the fundamental of the Odin Project. This is a simple calculator web application built with HTML, CSS, and JavaScript. The calculator allows users to perform basic arithmetic operations such as addition, subtraction, multiplication, and division.
+
+## Features
+
+- **Responsive Design**: The calculator is designed to work well on various screen sizes, making it accessible on both desktop and mobile devices.
+
+- **Arithmetic Operations**: Perform basic arithmetic calculations, including addition, subtraction, multiplication, and division.
+
+- **Decimal Support**: Use the decimal point button to input and calculate decimal numbers.
+
+- **Delete Functionality**: Easily delete the last input or clear the entire calculation with the delete button.
+
+- **Negative Numbers**: Toggle the sign of the current input with the minus button.
+
+## Getting Started
+
+To use the calculator, simply open the `index.html` file in your web browser. The calculator interface will be displayed, allowing you to interact with the buttons and perform calculations.
+
+## Usage
+
+- **Number Buttons (0-9)**: Click on the number buttons to input numeric values.
+
+- **Operator Buttons (+, -, x, /)**: Use these buttons to select the desired arithmetic operation.
+
+- **Equal Button (=)**: Press the equal button to calculate the result of the entered expression.
+
+- **Decimal Button (.)**: Add a decimal point to the current input for decimal numbers.
+
+- **Delete Button (DEL)**: Remove the last digit entered or clear the entire input.
+
+- **Clear Button (C)**: Clear the entire calculation and start over.
+
+- **Minus Button (-)**: Toggle the sign of the current input to make it negative or positive.
+
+## Code Structure
+
+The JavaScript code for this calculator is organized into functions and event listeners. Here are some key components:
+
+- **Variables**: Global variables hold the current and previous values, operator, and result.
+
+- **Event Listeners**: Functions are triggered by button clicks, handling numeric input, operators, and special functions.
+
+- **Operate Function**: Performs the actual calculation based on the selected operator.
+
+## Contributing
+
+Feel free to contribute to the project by opening issues or submitting pull requests. Bug reports, feature requests, and improvements are welcome!
+
+## License
+
+This calculator app is open-source and available under the [MIT License](LICENSE). Feel free to use, modify, and distribute the code as needed.
---
+++ main.js
@@ -0,0 +1,149 @@
+const previousDisplay = document.querySelector(".previous__display");
+const currentDisplay = document.querySelector(".current__display");
+const number = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector(".clear");
+const equalBtn = document.querySelector(".equal");
+const dotBtn = document.querySelector(".dot");
+const deleteBtn = document.querySelector(".delete");
+const minusBtn = document.querySelector(".minus");
+//Global Variable
+let currentValue = "";
+let previousValue = "";
+let operatorValue = "";
+let result = "";
+
+number.forEach(num => num.addEventListener("click", handleNumber));
+
+function handleNumber(event) {
+  if (currentValue.length > 5) {
+    currentValue = currentValue.slice(0, 5);
+    currentDisplay.textContent = currentValue;
+  }
+  let inputNumber = event.target.textContent;
+  currentValue += inputNumber;
+  currentDisplay.textContent = currentValue;
+}
+
+//Handle operator
+operator.forEach(op => {
+  op.addEventListener("click", handleOperator);
+});
+
+function handleOperator(event) {
+  let inputOperator = event.target.textContent;
+
+  if (previousDisplay.textContent.includes("=") && currentValue !== "") {
+    previousDisplay.textContent = currentValue;
+  }
+
+  if (previousValue !== "") {
+    operatorValue = inputOperator;
+    previousDisplay.textContent = `${previousValue} ${operatorValue}`;
+  }
+
+  if (previousValue === "" && currentValue !== "") {
+    operatorValue = inputOperator;
+    previousValue = currentValue;
+    previousDisplay.textContent = previousValue + " " + operatorValue;
+    currentDisplay.textContent = "0";
+    currentValue = "";
+  } else if (previousValue !== "" && currentValue !== "") {
+    previousValue = operate().toString();
+    operatorValue = inputOperator;
+    console.log("run", previousValue.length);
+
+    if (previousValue.length > 5) {
+      console.log("run");
+      // previousValue = previousValue.toString().slice(0, 5) + "...";
+      previousDisplay.textContent = `${previousValue
+        .toString()
+        .slice(0, 5)}... ${operatorValue}`;
+    } else {
+      previousDisplay.textContent = `${previousValue
+        .toString()
+        .slice(0, 5)}... ${operatorValue}`;
+    }
+
+    currentDisplay.textContent = "0";
+    currentValue = "";
+  }
+}
+
+clearBtn.addEventListener("click", () => {
+  currentValue = "";
+  previousValue = "";
+  operatorValue = "";
+  previousDisplay.textContent = "";
+  currentDisplay.textContent = "0";
+});
+
+//Calculate value
+function operate() {
+  let result;
+  switch (operatorValue) {
+    case "+":
+      return parseInt(previousValue) + parseInt(currentValue);
+      break;
+    case "-":
+      return parseInt(previousValue) - parseInt(currentValue);
+      break;
+    case "/":
+      return parseInt(previousValue) / parseInt(currentValue);
+      break;
+    case "x":
+      return parseInt(previousValue) * parseInt(currentValue);
+      break;
+    default:
+      return;
+  }
+}
+
+//Handle equal button
+equalBtn.addEventListener("click", () => {
+  result = operate()
+    .toString()
+    .slice(0, 5);
+
+  if (result.length === 5) {
+    result += "...";
+  }
+
+  if (previousDisplay.textContent.includes("=")) {
+    return;
+  }
+  previousDisplay.textContent = `${previousValue} ${operatorValue} ${currentValue} =  ${result}`;
+  currentDisplay.textContent = result;
+  currentValue = "";
+});
+
+dotBtn.addEventListener("click", () => {
+  if (currentValue === "") {
+    currentValue = "0.";
+    currentDisplay.textContent = currentValue;
+  } else if (currentValue !== "" && !currentValue.includes(".")) {
+    currentValue = currentValue + ".";
+    currentDisplay.textContent = currentValue;
+  }
+});
+
+//Handle delete
+deleteBtn.addEventListener("click", () => {
+  if (currentValue === "") {
+    return;
+  } else if (currentValue.length === 1 && currentValue !== "") {
+    currentValue = "";
+    currentDisplay.textContent = "0";
+  } else {
+    currentValue = currentValue.slice(0, -1);
+    currentDisplay.textContent = currentValue;
+  }
+});
+
+//Handle minus
+minusBtn.addEventListener("click", () => {
+  if (currentValue !== "" && !currentValue.includes("-")) {
+    currentValue = "-" + currentValue;
+    currentDisplay.textContent = currentValue;
+  }
+});
